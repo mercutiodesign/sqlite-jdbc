fdc52ff5f1a0ed4b3c569d380b4c2a0392fc5440
Martin D <luma.inc@gmail.com>
2017-10-10 22:37:53 +0200

    disable publishing travis builds to maven

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -5,11 +5,7 @@ VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non
 
 # Deploy a snapshot version only for master branch and jdk8
 if [[ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]]; then 
-  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$VERSION" == *SNAPSHOT ]]; then
-     make && mvn -s settings.xml deploy -DskipTests;
-  else
-    make;
-  fi;
+  make;
 else
   make linux64 && mvn test;
 fi;
